0bade73ee3c1ae33000f8f5b4b4c397309744888
cornholio11111 <conine999990@gmail.com>
2024-10-22 15:28:16 -0400

    update

--- Sellow_HUB/Application.cpp
+++ Sellow_HUB/Application.cpp
@@ -1,7 +1,23 @@
 #include "Application.h"
 
-int main(void) {
+#include <imgui/imgui.h>
+#include <imgui/imgui_impl_glfw.h>
+#include <imgui/imgui_impl_opengl3.h>
+
+// TODO: Add messages handling
+
+#if defined(_MSC_VER) && (_MSC_VER >= 1900) && !defined(IMGUI_DISABLE_WIN32_FUNCTIONS)
+#pragma comment(lib, "legacy_stdio_definitions")
+#endif
+
+static void glfw_error_callback(int error, const char* description)
+{
+    fprintf(stderr, "GLFW Error %d: %s\n", error, description);
+}
+
+int main(int, char**) {
     // Initialize GLFW
+    glfwSetErrorCallback(glfw_error_callback);
     if (!glfwInit()) {
         fprintf(stderr, "Failed to initialize GLFW\n");
         return -1;
@@ -22,14 +38,44 @@ int main(void) {
     // Make the window's context current
     glfwMakeContextCurrent(window);
 
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
+
+    // imgui Context
+    IMGUI_CHECKVERSION();
+    ImGui::CreateContext();
+    ImGuiIO& io = ImGui::GetIO(); (void)io;
+    io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;     // Enable Keyboard Controls
+    io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;      // Enable Gamepad Controls
+
+    // Setup Dear ImGui style
+    ImGui::StyleColorsDark();
+    //ImGui::StyleColorsLight();
+
+    // Setup Platform/Renderer backends
+    ImGui_ImplGlfw_InitForOpenGL(window, true);
+
     // Main loop
     while (!glfwWindowShouldClose(window)) {
-        // Render here (if you want to add rendering code)
+        // ImGui Start
+        ImGui_ImplGlfw_NewFrame();
+        ImGui_ImplOpenGL3_NewFrame();
+        ImGui::NewFrame();
+        ImGui::ShowDemoWindow();
+
+        // Render here
+        ImGui::Render();
+        ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
 
         // Swap buffers
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
+
+    // Shutdown
+    ImGui_ImplGlfw_Shutdown();
+    ImGui_ImplOpenGL3_Shutdown();
+    ImGui::DestroyContext();
 
     // Clean up and close the window
     glfwDestroyWindow(window);
--- Sellow_HUB/Application.h
+++ Sellow_HUB/Application.h
@@ -1,21 +1,22 @@
 #pragma once
 #include <stdio.h>
 #include <iostream>
+#include <stdio.h>
 
 // APIs
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
+
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
-#include "imgui.h"
-#include "imgui_impl_glfw.h"
-#include "imgui_impl_opengl3.h"
 
-// Assets
-//#include "../Shared/vender/imgui/imgui.h"
-//#include "../Shared/vender/imgui/imgui_impl_glfw.h"
-//#include "../Shared/vender/imgui/imgui_impl_opengl3.h"
+#include <imgui/imgui.h>
+#include <imgui/imgui_impl_glfw.h>
+#include <imgui/imgui_impl_opengl3.h>
 
 // Configs
 const char* ApplicationTitle = "Sellow HUB";
+
+// Backend
+const char* glsl_version = "#version 130";
