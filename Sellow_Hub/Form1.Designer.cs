0838dc8a775a732b306d86f471cae49d70cd3a8a
cornholio11111 <conine999990@gmail.com>
2024-10-23 15:08:04 -0400

    new hub?

---
+++ Sellow_Hub/Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace Sellow_Hub
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.GameList = new System.Windows.Forms.ListView();
+            this.mainFrameBarManager1 = new Syncfusion.Windows.Forms.Tools.XPMenus.MainFrameBarManager(this);
+            this.bar1 = new Syncfusion.Windows.Forms.Tools.XPMenus.Bar(this.mainFrameBarManager1, "File");
+            ((System.ComponentModel.ISupportInitialize)(this.mainFrameBarManager1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // GameList
+            // 
+            this.GameList.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(43)))), ((int)(((byte)(45)))), ((int)(((byte)(49)))));
+            this.GameList.ForeColor = System.Drawing.Color.White;
+            this.GameList.HideSelection = false;
+            this.GameList.Location = new System.Drawing.Point(-1, 62);
+            this.GameList.MultiSelect = false;
+            this.GameList.Name = "GameList";
+            this.GameList.Size = new System.Drawing.Size(116, 388);
+            this.GameList.TabIndex = 4;
+            this.GameList.TileSize = new System.Drawing.Size(112, 30);
+            this.GameList.UseCompatibleStateImageBehavior = false;
+            this.GameList.View = System.Windows.Forms.View.Tile;
+            this.GameList.SelectedIndexChanged += new System.EventHandler(this.listView1_SelectedIndexChanged);
+            // 
+            // mainFrameBarManager1
+            // 
+            this.mainFrameBarManager1.BarPositionInfo = ((System.IO.MemoryStream)(resources.GetObject("mainFrameBarManager1.BarPositionInfo")));
+            this.mainFrameBarManager1.Bars.Add(this.bar1);
+            this.mainFrameBarManager1.CurrentBaseFormType = "";
+            this.mainFrameBarManager1.EnableMenuMerge = true;
+            this.mainFrameBarManager1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.mainFrameBarManager1.Form = this;
+            this.mainFrameBarManager1.MetroBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(31)))), ((int)(((byte)(34)))));
+            this.mainFrameBarManager1.MetroColor = System.Drawing.Color.FromArgb(((int)(((byte)(67)))), ((int)(((byte)(201)))), ((int)(((byte)(232)))));
+            this.mainFrameBarManager1.Office2010Theme = Syncfusion.Windows.Forms.Office2010Theme.Managed;
+            this.mainFrameBarManager1.ResetCustomization = false;
+            this.mainFrameBarManager1.Style = Syncfusion.Windows.Forms.VisualStyle.Default;
+            this.mainFrameBarManager1.ThemeName = "Default";
+            this.mainFrameBarManager1.UseBackwardCompatiblity = false;
+            // 
+            // bar1
+            // 
+            this.bar1.BarName = "File";
+            this.bar1.Caption = "File";
+            this.bar1.Manager = this.mainFrameBarManager1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(49)))), ((int)(((byte)(51)))), ((int)(((byte)(56)))));
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.GameList);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.mainFrameBarManager1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.ListView GameList;
+        private Syncfusion.Windows.Forms.Tools.XPMenus.MainFrameBarManager mainFrameBarManager1;
+        private Syncfusion.Windows.Forms.Tools.XPMenus.Bar bar1;
+    }
+}
+
